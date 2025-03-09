a1b19de9f643dbbe4de265ef697e5c89c8eeca6e
Sein <hosein.mufti@gmail.com>
2025-03-09 13:27:07 +0300

    removed debugging

--- app.py
+++ app.py
@@ -104,7 +104,6 @@ def update_pr():
         "title": pr_title,
         "total_cost": float(pr_total_cost),
     }
-    return revised_pr_logs
 
     revised_pr_entries = []
     for i in range(len(pr_indices)):
